3a41f2faa45a08761905a929608a336a6f60061d
Lutif <lmandhro@gmail.com>
2019-11-19 19:36:08 +0500

    completed footer header contact about

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 // jshint esversion:6
+//adding some text constants
+const homeStringConst = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis doloremque eos esse? Qui iste ipsam quia aperiam reprehenderit, nam, temporibus mollitia adipisci molestiae eveniet quisquam nesciunt maxime nobis tenetur placeat.';
+const aboutStringConst = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. mollitia adipisci molestiae eveniet quisquam nesciunt maxime nobis tenetur placeat.';
+const contactStringConst = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. mollitia adipisci molestiae';
+
+let postlist=[];
+postlist[0]={
+    heading: 'Day 1',
+    body:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis doloremque eos esse? Qui iste ipsam quia aperiam reprehenderit, nam, temporibus mollitia adipisci molestiae eveniet quisquam nesciunt maxime nobis tenetur placeat.'
+         }
+postlist[1]={
+    heading: 'Day 2',
+    body:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis doloremque eos esse? Qui iste ipsam quia aperiam reprehenderit, nam, temporibus mollitia adipisci molestiae eveniet quisquam nesciunt maxime nobis tenetur placeat.'
+         }
+
 
 //Requiring modules
 const express = require ('express');
@@ -13,12 +28,37 @@ const app = express();
 app.use(express.static('public'));
 
 //telling express to use bodyParser to parse response body
-app.use('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine','ejs' );
 
 
 app.get('/', function (req,res) {
-    
+    res.render('home' , {content:homeStringConst, posts: postlist});
 });
+
+app.get('/about', function (req,res) {
+    console.log('rendering about');
+    res.render('about' , {content:aboutStringConst});
+});
+app.get('/contact', function (req,res) {
+    res.render('contact' , {content:contactStringConst});
+});
+
+app.get('/compose', function(req, res){
+    res.render('compose');
+});
+
+app.post('/compose', function (req, res) {
+    let title=req.body.title;
+    let postBody= req.body.post;
+    let newPost = {
+        heading: title,
+        body: postBody
+    }
+    postlist.push(newPost);
+    res.redirect('/');
+})
+
 
 
 
